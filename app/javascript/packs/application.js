a9a208c37f84670eb7e30429e507a8be384d4f88
suaib <mohd.suaib786@gmail.com>
2021-09-12 10:58:36 +0530

    some ui fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("packs/direct_upload")
-require("packs/jquery_min")
+// require("packs/jquery_min")
 // require("packs/bootstrap.min")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- app/models/file_record.rb
+++ app/models/file_record.rb
@@ -1,5 +1,10 @@
 class FileRecord < ApplicationRecord
     belongs_to :user
     has_one_attached :file
+    validates :title,presence: :true
+    validates :description, presence: :true
     #validates :file, content_type: ["image/png", "image/jpeg"] #content_type: %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)
+
+
+   
 end
